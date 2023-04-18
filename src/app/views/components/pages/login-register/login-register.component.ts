50b49bf533e8db93564bbb69035e79b4edc53df4
Ahmed-dev-dragon <aouladoumarahmed100@gmail.com>
2023-04-18 13:32:24 +0000

    commit<any[]>

--- src/app/views/components/pages/login-register/login-register.component.ts
+++ src/app/views/components/pages/login-register/login-register.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component,  } from '@angular/core';
 
 @Component({
     selector: "app-login-register",
     templateUrl: "./login-register.component.html",
     styleUrls: ["./login-register.component.scss"],
+    standalone: true,
+    imports: [CommonModule],
 })
 export class LoginRegisterComponent {
     constructor() {}
-
     ngOnInit(): void {}
 }
